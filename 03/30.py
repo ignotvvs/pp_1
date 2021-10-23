b83c9264c6a29d4d31defa6fb958d9062688191c
Iga <s221480@student.uek.krakow.pl>
2021-10-23 12:04:18 +0200

    30 complete,31 add

--- 03/30.py
+++ 03/30.py
@@ -1,3 +1,25 @@
-n = int(input("Enter n: "))
-for i in range(2,n**0.5):
-    
+wariant = 1
+# 1 wariant
+if wariant == 1:
+    n = int(input("Enter n: "))
+    pomoc = False
+    for i in range(2,n+1):
+        for j in range(2,int(i**0.5)+1):
+            if i%j==0: pomoc = True
+        if pomoc == False: print(i,end="  ")
+        pomoc = False
+
+# 2 wariant
+def pierwsze(s):
+    if s<2:return 0
+    for i in range(2,int(s**0.5)+1):
+        if s%i==0: return 0
+    return 1
+if wariant == 2:
+    n = int(input("Enter n: "))
+    prime = []
+    x=2
+    while len(prime)<n:
+        if pierwsze(x) == 1: prime.append(x)
+        x+=1
+    print(prime)
