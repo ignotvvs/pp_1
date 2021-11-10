bdb8c3b54233e0c04fb9daae57ebef48de4844d7
Ignotus <s221480@student.uek.krakow.pl>
2021-11-10 18:01:29 +0100

    renames ;P

--- 06/array_11.py
+++ 06/array_11.py
@@ -1,10 +1,10 @@
-import I0
+import array_10
 def compare(array1,array2):
     if array1 == array2:
             return True
     return False
 array1 = ["water","book","sky"]
 array2 = ["water","boo0k","sky"]
-print(f"Array1: {I0.array2str(array1)}")
-print(f"Array2: {I0.array2str(array2)}")
+print(f"Array1: {array_10.array2str(array1)}")
+print(f"Array2: {array_10.array2str(array2)}")
 print("Comparison: arrays are the same") if compare(array1,array2) == True else print("Comparison: not the same")
